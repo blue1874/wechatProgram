4f0a93fc329928762852f581f1f3e3bd6f864bdc
blue <bluelizhijian@gmail.com>
2019-04-02 14:06:17 +0800

    第一课界面
    rt

--- pages/menu/menu.js
+++ pages/menu/menu.js
@@ -5,68 +5,118 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    active : false,
   },
   //my funtions
-  gotoKanaLearn:function(){
+  setActive:function()
+  {
+    if(this.data.active)
+    {
+      this.setData({
+        active: false,
+      })
+    }
+    else
+    {
+      this.setData({
+        active: true,
+      })
+    }
+  },
+  back:function()
+  {
     wx.redirectTo({
-      url:"../kanaLearn/select/select",
+      url: '/pages/index/index',
     })
   },
-
+  gotoKanaLearn:function()
+  {
+    wx.redirectTo({
+      url: '../kanaLearn/select/select',
+      })
+  },
+  gotoTestFunction:function()
+  {
+    wx.redirectTo({
+      url: '../test/test1',
+      })
+  },
+  gotoFirstSection:function()
+  {
+    wx.redirectTo({
+      url: '../context/theFirstUnit/theFirstSection',
+    })
+  },
+  gotoSecondSection: function () {
+    wx.redirectTo({
+      url: '../context/theFirstUnit/theSecondSection',
+    })
+  },
+  gotoThirdSection: function () {
+    wx.redirectTo({
+      url: '../context/theFirstUnit/theThirdSection',
+    })
+  },
+  gotoFourthSection: function () {
+    wx.redirectTo({
+      url: '../context/theFirstUnit/theFourthSection',
+    })
+  },
+  gotoSecondUnit: function () {
+    wx.redirectTo({
+      url: '../context/theSecondUnit/theFirstSection',
+    })
+  },
+  gotoThirdUnit: function () {
+    wx.redirectTo({
+      url: '../context/theThirdUnit/theFirstSetion',
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  
-  },
+  onLoad: function (options)
+  {},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
-  },
+  onReady: function () 
+  {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  
-  },
+  onShow: function () 
+  {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-  
-  },
+  onHide: function () 
+  {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-  
-  },
+  onUnload: function () 
+  {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-  
-  },
+  onPullDownRefresh: function () 
+  {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-  
-  },
+  onReachBottom: function () 
+  {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-  
-  }
+  onShareAppMessage: function () 
+  {}
 })
